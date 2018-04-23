f8d2973c465c9f3f11d4d8930644f8e5d7c8f2ff
ABardot <aabardot5@gmail.com>
2018-04-23 19:11:18 -0400

    OOP Auto practice

--- CSharpClassesPractice/CSharpClassesPractice/Program.cs
+++ CSharpClassesPractice/CSharpClassesPractice/Program.cs
@@ -5,30 +5,10 @@ namespace CSharpClassesPractice
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Would you prefer to be behind door number 1, 2, or 3?");
-            string userValue = Console.ReadLine();
-
-            string message = "";
-            if (userValue == "1")
-            {                  
-                message = "You have won a car!";
-            }
-            else if (userValue == "2")
-            {
-                message = "You win a boat!";
-            }
-            else if (userValue == "3")
-            {
-                message = "You won an airplane!";
-            }
-            else
-            {
-                message = "Sorry you didn't win a prize.";
-            }
-
-            Console.WriteLine(message);
-        }
+        
+           
+        } 
     }
 }
