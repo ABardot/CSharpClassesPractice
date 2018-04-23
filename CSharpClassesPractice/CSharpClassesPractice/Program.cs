3f55081b20af153b8eb27c404e468d11cb65add4
ABardot <aabardot5@gmail.com>
2018-04-23 19:21:08 -0400

    Practicing calling my methods

--- CSharpClassesPractice/CSharpClassesPractice/Program.cs
+++ CSharpClassesPractice/CSharpClassesPractice/Program.cs
@@ -7,8 +7,22 @@ namespace CSharpClassesPractice
     {
         static void Main()
         {
-        
-           
+            Auto myCar = new Auto();
+            myCar.Speed = 100;
+            myCar.Make = "BMW";
+            myCar.Model = "M3";
+            myCar.Year = 2018;
+            myCar.Color = "Black";
+            myCar.Miles = 10000;
+
+            Console.WriteLine(myCar.Make  + " " + myCar.Model);
+
+            //myCar.Accelerate();
+            //myCar.Accelerate();
+            //myCar.Accelerate();
+            //myCar.Decelerate();
+
+            Console.WriteLine("My current speed: " + myCar.Speed);
         } 
     }
 }
